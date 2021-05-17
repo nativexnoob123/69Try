e244642a402c1820a0ae04424344df5e887a900b
nativexnoob123 <82684674+nativexnoob123@users.noreply.github.com>
2021-05-17 08:34:33 +0530

    Update ping.py

--- 69/modules/ping.py
+++ 69/modules/ping.py
@@ -5,7 +5,7 @@ from datetime import datetime
 @app.on_message(filters.command("ping"))
 async def ping(_, message):
     start = datetime.now()
-    response = await message.reply_text("WAIT BRO")
+    response = await message.reply_text("Ping!")
     end = datetime.now()
     resp = (end - start).microseconds / 1000
-    await response.edit_text(f"**Pong!**\n`⚡{resp} ms`\n\n<b><u>Yukki System Stats:</u></b>{uptime}")   
+    await response.edit_text(f"**Pong!**\n`{resp} ms`")   
