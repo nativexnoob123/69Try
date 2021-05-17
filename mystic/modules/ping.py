ff6aa4662d9290086b243670a68372d4f677c123
nativexnoob123 <82684674+nativexnoob123@users.noreply.github.com>
2021-05-17 09:38:43 +0530

    Update ping.py

--- mystic/modules/ping.py
+++ mystic/modules/ping.py
@@ -1,4 +1,4 @@
-from mystic import app2
+from mystic import app
 from datetime import datetime
 from pyrogram import filters, Client
 
@@ -6,8 +6,10 @@ __MODULE__ = "Ping"
 __HELP__ = "Pong."
 
 
-@app2.on_message(filters.command("ping"))
-async def ping(client, message):
+@app.on_message(
+    filters.command("ping") & ~filters.edited & ~filters.private
+)
+async def save_filters(_, message):
     start = datetime.now()
     response = await message.reply_text("Ping!")
     end = datetime.now()
