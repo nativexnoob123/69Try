bca0845a7baffaed2af3ebf545a66688708d3896
nativexnoob123 <82684674+nativexnoob123@users.noreply.github.com>
2021-05-17 09:31:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,18 @@
+HEROKU = False  # NOTE Make it false if you're not deploying on heroku.
+
+
 BOT_TOKEN = "1854281737:AAEN3nfmTIDdgCsfdjrfDE9MWa_How5lfs8"
 API_ID = 3277132
 API_HASH = "50ad2ffad01bf10049638526009661a8"
+USERBOT_PREFIX = "."
+PHONE_NUMBER = "+919044564845" # Need for Helper Userbot
+SUDO_USERS_ID = [1047091391, 1562091466, 970558707] # Sudo users have full access to everythin, don't trust anyone
+LOG_GROUP_ID = -1001266722072
+GBAN_LOG_GROUP_ID = -1001266722072
+MESSAGE_DUMP_CHAT = -1001266722072
+FERNET_ENCRYPTION_KEY = "iKMq0WZMnJKjMQxZWKtv-cplMuF_LoyshXj0XbTGGWM=" # Leave this as it is
+WELCOME_DELAY_KICK_SEC = 300
+MONGO_DB_URI = "mongodb+srv://abc:abc@cluster0.ye0s8.mongodb.net/test?retryWrites=true&w=majority"
+SPAMWATCH_API_KEY = "Je~p3De8ABMTLiU7CU~NvraDLNuh_21NIBZtT4gdizePC7QzDICEqPt_qN2um2n8"
+# NOTE Don't make changes below this line
+ARQ_API_BASE_URL = "http://thearq.tech"
