90c82d672710071eaa31abf6bdc3fc71bfb13eca
nativexnoob123 <82684674+nativexnoob123@users.noreply.github.com>
2021-05-17 09:13:16 +0530

    Update ping.py

--- mystic/modules/ping.py
+++ mystic/modules/ping.py
@@ -8,7 +8,7 @@ __HELP__ = "Pong."
 @app.on_message(filters.command("ping"))
 async def ping(_, message):
     start = datetime.now()
-    response = await app.send_message(message.chat.id, Ping!)
+    response = await app.send_message(message.chat.id, "Ping!")
     end = datetime.now()
     resp = (end - start).microseconds / 1000
     await response.edit_message(message.chat.id, f"**Pong!**\n`{resp} ms`")  
