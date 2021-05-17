223088f528105e3d832afb73d8b7b5c801573ccf
nativexnoob123 <82684674+nativexnoob123@users.noreply.github.com>
2021-05-17 08:40:47 +0530

    Update __init__.py

--- 69/__init__.py
+++ 69/__init__.py
@@ -1,15 +1,20 @@
 from pyrogram import Client
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 import time
-
+is_config = path.exists('config.py')
+if is_config:
+    from config import *
+else:
+    from sample_config import *
+    
 bot_start_time = time.time()
 MOD_LOAD = []
 MOD_NOLOAD = []
 app = Client(
     "yukki",
-    bot_token=1661608026:AAE0tqCI64fjYdPmVnDhNIbSUDo1KQJ6GU4,
-    api_id=3277132,
-    api_hash=50ad2ffad01bf10049638526009661a8
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH
 )
 
 app.start()
