a7f1041cbbd7b6b550867a166b845c0d0ea7361c
nativexnoob123 <82684674+nativexnoob123@users.noreply.github.com>
2021-05-17 08:53:43 +0530

    Create ping.py

---
+++ mystic/modules/ping.py
@@ -0,0 +1,11 @@
+from mystic import app
+from datetime import datetime
+
+
+@app.on_message(filters.command("ping"))
+async def ping(_, message):
+    start = datetime.now()
+    response = await message.reply_text("Ping!")
+    end = datetime.now()
+    resp = (end - start).microseconds / 1000
+    await response.edit_text(f"**Pong!**\n`{resp} ms`")  
