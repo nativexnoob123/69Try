8802f96ab64488fbab2b8752b7db256dd4e7bb41
nativexnoob123 <82684674+nativexnoob123@users.noreply.github.com>
2021-05-17 09:17:53 +0530

    Update ping.py

--- mystic/modules/ping.py
+++ mystic/modules/ping.py
@@ -1,14 +1,15 @@
 from mystic import app
 from datetime import datetime
-from pyrogram import filters
+from pyrogram import filters, Client
+
 __MODULE__ = "Ping"
 __HELP__ = "Pong."
 
 
 @app.on_message(filters.command("ping"))
-async def ping(_, message):
+async def ping(client, message):
     start = datetime.now()
-    response = await app.send_message(message.chat.id, "Ping!")
+    response = await message.reply_text("Ping!")
     end = datetime.now()
     resp = (end - start).microseconds / 1000
-    await response.edit_message(message.chat.id, f"**Pong!**\n`{resp} ms`")  
+    await response.edit_text(f"**Pong!**\n`{resp} ms`")  
