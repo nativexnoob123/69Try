4a5e5e9ab266a98c1ffc8bd90ad33aafea7dc7f5
nativexnoob123 <82684674+nativexnoob123@users.noreply.github.com>
2021-05-17 08:50:05 +0530

    Create __init__.py

---
+++ mystic/__init__.py
@@ -0,0 +1,22 @@
+from pyrogram import Client
+import time
+from os import path
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+import time
+is_config = path.exists('config.py')
+if is_config:
+    from config import *
+else:
+    from sample_config import *
+    
+bot_start_time = time.time()
+MOD_LOAD = []
+MOD_NOLOAD = []
+app = Client(
+    "yukki",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH
+)
+
+app.start()
