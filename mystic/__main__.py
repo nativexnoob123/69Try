c4aa4e05ea22071b4144f9b8b6a1478781202890
nativexnoob123 <82684674+nativexnoob123@users.noreply.github.com>
2021-05-17 09:06:08 +0530

    Update __main__.py

--- mystic/__main__.py
+++ mystic/__main__.py
@@ -12,12 +12,31 @@ async def start_bot():
     global COMMANDS_COUNT
     for module in ALL_MODULES:
         imported_module = importlib.import_module("mystic.modules." + module)
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+        if (
+            hasattr(imported_module, "__MODULE__")
+            and imported_module.__MODULE__
+        ):
             imported_module.__MODULE__ = imported_module.__MODULE__
-            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+            if (
+                hasattr(imported_module, "__HELP__")
+                and imported_module.__HELP__
+            ):
                 HELPABLE[imported_module.__MODULE__.lower()] = imported_module
     bot_modules = ""
-    print(f"[INFO]: BOT STARTED!")
+    j = 1
+    for i in ALL_MODULES:
+        if j == 4:
+            bot_modules += "|{:<15}|\n".format(i)
+            j = 0
+        else:
+            bot_modules += "|{:<15}".format(i)
+        j += 1
+    print("+===============================================================+")
+    print("|                              WBB                              |")
+    print("+===============+===============+===============+===============+")
+    print(bot_modules)
+    print("+===============+===============+===============+===============+")
+    print(f"BOT STARTED")
     await idle()
 
     
