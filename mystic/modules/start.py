bfe21287a85b857cc15a9d7af18f561dcdb51db8
nativexnoob123 <82684674+nativexnoob123@users.noreply.github.com>
2021-05-17 09:02:26 +0530

    Update start.py

--- mystic/modules/start.py
+++ mystic/modules/start.py
@@ -1,6 +1,10 @@
 from mystic import app
 from pyrogram import filters
 
+__MODULE__ = "Start"
+__HELP__ = "•Anime uwu•\n\n/anime - search anime on AniList\n /manga - search manga on Anilist\n /char - search character on Anilist\n /nhentai ID - returns the nhentai in telegraph instant preview format."
+
+
 @app.on_message(filters.command("start"))
 async def start(_, message):
     await message.reply_text("Hi Vro")
