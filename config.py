a6a5f39bca66afe09c67b0c7f414c62a46eb1ec1
nativexnoob123 <82684674+nativexnoob123@users.noreply.github.com>
2021-06-14 00:25:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ GBAN_LOG_GROUP_ID = -1001266722072
 MESSAGE_DUMP_CHAT = -1001266722072
 FERNET_ENCRYPTION_KEY = "iKMq0WZMnJKjMQxZWKtv-cplMuF_LoyshXj0XbTGGWM=" # Leave this as it is
 WELCOME_DELAY_KICK_SEC = 300
-MONGO_DB_URI = "mongodb+srv://abc:abc@cluster0.ye0s8.mongodb.net/test?retryWrites=true&w=majority"
+MONGO_DB_URI = "mongodb+srv://abc:abc@cluster0.w2rwt.mongodb.net/Test?retryWrites=true&w=majority"
 SPAMWATCH_API_KEY = "Je~p3De8ABMTLiU7CU~NvraDLNuh_21NIBZtT4gdizePC7QzDICEqPt_qN2um2n8"
 # NOTE Don't make changes below this line
 ARQ_API_BASE_URL = "http://thearq.tech"
