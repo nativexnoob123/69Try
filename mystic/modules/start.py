2652a6ce124fb8c98c21a8d1714da79278b0c479
nativexnoob123 <82684674+nativexnoob123@users.noreply.github.com>
2021-06-13 06:34:01 +0530

    Update start.py

--- mystic/modules/start.py
+++ mystic/modules/start.py
@@ -179,7 +179,7 @@ async def start(_, message):
 @app.on_message(filters.command("play"))
 async def start(_, message):
     user_id = message.from_user.id
-    if await is_dec_on(message.chat.id):
+    if not await is_dec_on(message.chat.id):
         await message.reply_text(f"You have been banned from Yukki due to Spam Activities.\n\n**Ban Unlocks In:** {que} seconds") 
         return
     file_path = time.time()
