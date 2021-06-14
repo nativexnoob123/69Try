f3584697fb87247e9f2b1f372d279624055128b5
nativexnoob123 <82684674+nativexnoob123@users.noreply.github.com>
2021-06-14 13:49:10 +0530

    Update ping.py

--- mystic/modules/ping.py
+++ mystic/modules/ping.py
@@ -139,7 +139,7 @@ async def save_blacklist_filter(chat_id: int, word: str):
 
 
 async def delete_blacklist_filter(chat_id: int, word: str) -> bool:
-    filtersd = awaitget_allafk_users(chat_id)
+    filtersd = await get_allafk_users(chat_id)
     word = word.lower().strip()
     if word in filtersd:
         filtersd.remove(word)
